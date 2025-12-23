b598c50111247e8dce798c54f506b1d0921c62df
mahdiyarKoushki <mkoushki.79@gmail.com>
2025-12-23 15:36:51 -0800

    add html

--- tts.py
+++ tts.py
@@ -2,17 +2,32 @@ import asyncio
 import edge_tts
 import tempfile
 import os
+from enum import Enum
 from aiohttp import TCPConnector
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+
+class Voice(str, Enum):
+    farid_persian_female = "fa-IR-FaridNeural"
+    aria_english_female = "en-US-AriaNeural"
+    zira_english_female = "en-US-ZiraNeural"
+    david_english_male = "en-US-DavidNeural"
+    mark_english_male = "en-US-MarkNeural"
+    lori_persian_female = "fa-IR-LoriNeural"
+    golnaz_persian_female = "fa-IR-GolnazNeural"
+    dilara_turkish_female = "tr-TR-DilaraNeural"
+    # Add more as needed
 
 class TTSRequest(BaseModel):
     text: str
-    voice: str = "fa-IR-FaridNeural"
+    voice: Voice = Voice.farid_persian_female
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,7 +38,7 @@ app.add_middleware(
 )
 
 async def speak(text: str, voice: str = "fa-IR-FaridNeural") -> str:
-    connector = TCPConnector(ssl=False)
+    connector = TCPConnector()
 
     communicate = edge_tts.Communicate(
         text,
